22f2794ac804e8b1cdd06023fa501e08091172b9
ann116 <51899795+annn16@users.noreply.github.com>
2023-02-06 23:38:04 +0700

    Add files via upload
    John Smilga JS Udemy basic

---
+++ 1-Starter/01-vars.js
@@ -0,0 +1,20 @@
+/*
+## Variables #1
+
+1. create "firstName" and "last_name" variables
+2. assign your values
+3. create "address" variable and assign "main street" value to it
+4. re-assign address to "first street" later
+5. log all values in the console
+
+*/
+
+const firstName = "To";
+const last_name = "Ann116";
+
+let address = "main street";
+address = "first street";
+
+console.log(firstName);
+console.log(last_name);
+console.log(address);
